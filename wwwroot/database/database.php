5b5c7e22e066ef67d2c5c95a0a2e3734c2db8e2d
estherleah <estherleahmorrison@gmail.com>
2017-02-02 20:42:37 +0000

    added password to database connection

--- wwwroot/database/database.php
+++ wwwroot/database/database.php
@@ -8,7 +8,7 @@
 
 $hostname = "localhost";
 $username = "root";
-$password = "";
+$password = "root";
 $database = "social_network";
 
 // Create connection
@@ -18,5 +18,5 @@ $conn = mysqli_connect($hostname, $username, $password, $database);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-echo "Connected successfully";
+//echo "Connected successfully";
 ?>
