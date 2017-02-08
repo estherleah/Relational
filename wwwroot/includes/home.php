129b8d0f0c886c3632b87957884382bbabb9afdc
Marcus Wallbaum <marcus.wallbaum@gmail.com>
2017-02-08 13:38:19 +0000

    Implemented live search for users and friends of users

---
+++ wwwroot/includes/home.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Home</title>
+    <!-- Bootstrap -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <!-- Stylesheet -->
+    <link href="../css/style.css" rel="stylesheet">
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="../js/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="../js/bootstrap.min.js"></script>
+    <!-- Search -->
+    <script type="text/javascript" src="../js/search.js"></script>
+</head>
+
+<body>
+<!-- Content -->
+<div class="container">
+    <div class="col-*-*">
+        <div class="col-md-6 col-sm-offset-3">
+            <div class="text-center">
+                <h2>Logged in</h2><br/>
+                <input type="text" class="search" id="searchid" placeholder="Search SocialNet" /><br/>
+                <div id="result"></div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
