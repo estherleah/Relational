7e845176acf5736086d13467e782644cab7fd950
Rob Farthing <farthir@outlook.com>
2017-02-18 14:02:44 +0000

    Resetting blog styling to how it was before (please read comment)
    Also moved script to bottom of page for better loading. Bootstrap seems quite sensitive to playing with the layout so before I kept it as simple as possible. It seems the last changes broke the layout so I’ve reset it and added the changes I think you were trying to achieve (smaller, centred user image on previous posts).
    
    PS. Col width should add to 12 (which is why I’ve reset those). Also I’ve reset the header to a row as this keeps the layout consistent

--- wwwroot/blog.php
+++ wwwroot/blog.php
@@ -7,16 +7,11 @@ include 'header.php';
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <title>Blog</title>
-    <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css">
-    <script src="js/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
 </head>
 
 <?php
@@ -46,12 +41,12 @@ $blogResult = mysqli_query($conn, $blogSql);
 <body>
 <!-- Content -->
 <div class="container">
-    <div class="text-center">
+    <div class="row" id="entry">
         <div class="col-xs-12">
             <h2>Blog for <?php echo $name ?></h2>
         </div>
         <div class="col-xs-2">
-            <img src="<?php echo $profilephotoURL ?>" class="img-circle" width="100%"/>
+            <img src="<?php echo $profilephotoURL ?>" class="img-circle center-block" width="100%"/>
         </div>
         <div class="col-xs-10">
             <form method="post" action="includes/addblogpost.php">
@@ -67,10 +62,10 @@ $blogResult = mysqli_query($conn, $blogSql);
         while ($row = mysqli_fetch_assoc($blogResult)) {
             ?>
             <div class="row" id="previousposts">
-                <div class="col-xs-1 col-xs-offset-1">
-                    <img src="<?php echo $row["profilephotoURL"] ?>" class="img-circle" width="100%"/>
+                <div class="col-xs-2">
+                    <img src="<?php echo $row["profilephotoURL"] ?>" class="img-circle center-block" width="50%"/>
                 </div>
-                <div class="col-xs-9">
+                <div class="col-xs-10">
                     <b><?php echo $row["firstName"] . " " . $row["lastName"] ?></b>
                     <button class="btn btn-danger btn-xs pull-right" type="button">Delete</button>
                     <div class="text-muted">
@@ -86,5 +81,7 @@ $blogResult = mysqli_query($conn, $blogSql);
 
 </div>
 
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
 </body>
 </html>
