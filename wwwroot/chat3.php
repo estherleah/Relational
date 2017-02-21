942aa42f132ae29bc4087bb6bfb599b94620b286
Sherri <leejiayusherri@gmail.com>
2017-02-21 19:11:40 +0000

    very basic chat based on blog with some minor changes, will link the working files later

---
+++ wwwroot/chat3.php
@@ -0,0 +1,78 @@
+<?php
+include_once 'database/database.php';
+session_start();
+include 'header.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>CircleChat</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<?php
+
+$userIDEscaped = mysqli_real_escape_string($conn, $user);
+
+$userSql = "SELECT firstName, lastName, profilephotoURL
+              FROM user
+              WHERE userID = '$userIDEscaped';
+              ";
+$userResult = mysqli_query($conn, $userSql);
+
+if (mysqli_num_rows($userResult) === 1) {
+    $row = mysqli_fetch_assoc($userResult);
+    $fullName = $row["firstName"] . " " . $row["lastName"];
+    $profilephotoURL = $row["profilephotoURL"];
+}
+//select the message database
+$messageSQL = "SELECT message, date
+              FROM message AS b JOIN user AS u
+              ON b.userID = '$userIDEscaped' AND b.userID = u.userID
+              ORDER BY date ASC;
+              ";
+$messageResult = mysqli_query($conn, $messageSQL);
+?>
+
+<body>
+<!-- Content -->
+<div class="container">
+    <div class="row" id="entry">
+        <div class="col-xs-12">
+            <h2 class="text-center">Magic Circle Chat for <?php echo $name ?></h2>
+        </div>
+
+        <?php
+        if (mysqli_num_rows($messageResult) > 0) {
+            while ($row = mysqli_fetch_assoc($messageResult)) {
+                ?>
+                <div class="row" id="previousposts">
+
+                    <div class="col-xs-10">
+                        <b><?php echo $name ?></b>
+                        <div><?php echo $row["date"] . " --- " . $row["message"] ?></div>
+                    </div>
+                </div>
+                <?php
+            }
+        }
+        ?>
+
+        <div class="col-xs-10">
+            <form method="post" action="includes/chatProcess3.php">
+                <textarea name="post" class="form-control" rows='3' id="postText"></textarea>
+                <input class="btn btn-primary pull-right" type="submit" value="Send message">
+            </form>
+        </div>
+    </div>
+
+</div>
+
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
