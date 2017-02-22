b1ea46d3ebdd9ecd9d2c052c847d40e540315949
Rob Farthing <farthir@outlook.com>
2017-02-22 18:56:27 +0000

    Add photo collection page (incomplete)

---
+++ wwwroot/includes/initialisePhotos.php
@@ -0,0 +1,57 @@
+<?php
+
+
+
+// Adapted from https://docs.microsoft.com/en-us/azure/storage/storage-php-how-to-use-blobs
+/*
+require_once 'vendor/autoload.php';
+use WindowsAzure\Common\ServicesBuilder;
+use MicrosoftAzure\Storage\Blob\Models\CreateContainerOptions;
+use MicrosoftAzure\Storage\Blob\Models\PublicAccessType;
+use MicrosoftAzure\Storage\Common\ServiceException;
+
+$userIDEscaped = mysqli_real_escape_string($conn, $user);
+
+$connectionString = "DefaultEndpointsProtocol=[http|https];AccountName=[yourAccount];AccountKey=[yourKey]";
+
+// Create blob REST proxy.
+$blobRestProxy = ServicesBuilder::getInstance()->createBlobService($connectionString);
+
+
+// OPTIONAL: Set public access policy and metadata.
+// Create container options object.
+$createContainerOptions = new CreateContainerOptions();
+
+// Set public access policy. Possible values are
+// PublicAccessType::CONTAINER_AND_BLOBS and PublicAccessType::BLOBS_ONLY.
+// CONTAINER_AND_BLOBS:
+// Specifies full public read access for container and blob data.
+// proxys can enumerate blobs within the container via anonymous
+// request, but cannot enumerate containers within the storage account.
+//
+// BLOBS_ONLY:
+// Specifies public read access for blobs. Blob data within this
+// container can be read via anonymous request, but container data is not
+// available. proxys cannot enumerate blobs within the container via
+// anonymous request.
+// If this value is not specified in the request, container data is
+// private to the account owner.
+$createContainerOptions->setPublicAccess(PublicAccessType::CONTAINER_AND_BLOBS);
+
+try    {
+    // Create container.
+    $containerName = $collectionName + "-" + $userIDEscaped;
+    $blobRestProxy->createContainer($containerName, $createContainerOptions);
+}
+catch(ServiceException $e){
+    // Handle exception based on error codes and messages.
+    // Error codes and messages are here:
+    // http://msdn.microsoft.com/library/azure/dd179439.aspx
+    $code = $e->getCode();
+    $error_message = $e->getMessage();
+    echo $code.": ".$error_message."<br />";
+}
+*/
+
+
+ ?>
---
+++ wwwroot/photos.php
@@ -0,0 +1,70 @@
+<?php
+include_once 'database/database.php';
+session_start();
+include 'header.php';
+include 'includes/initialisePhotos.php';
+?>
+
+<!-- Layout adapted from https://github.com/BlackrockDigital/startbootstrap-thumbnail-gallery/blob/master/index.html -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Photos</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+  <!-- Content -->
+  <div class="container">
+    <div class="row">
+        <div class="col-xs-12">
+            <h1 class="page-header"><?php echo $name ?>'s Photo Collections</h1>
+        </div>
+    </div>
+    <div class="row">
+          <div class="col-xs-12">
+              <h3>Create a new collection</h3>
+          </div>
+    </div>
+    <div class="row">
+          <div class="col-xs-12">
+            <div class="form-group">
+              <label for="name">Name:</label>
+              <input type="text" class="form-control" id="name">
+            </div>
+            <button class="btn btn-primary pull-right" id="postSubmit" type="button">Add</button>
+          </div>
+    </div>
+
+    <div class="row">
+          <div class="col-xs-12">
+              <h3>Existing collections</h3>
+          </div>
+    </div>
+        <?php
+
+        ?>
+    <div class="row" id="existingCollections">
+        <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+            <a class="thumbnail" href="#">
+                <img class="img-responsive" src="http://placehold.it/400x300" alt="">
+            </a>
+        </div>
+    </div>
+  </div>
+  </div>
+
+
+
+
+  </div>
+
+  <script src="js/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/addPhotoCollection.js"></script>
+</body>
+</html>
