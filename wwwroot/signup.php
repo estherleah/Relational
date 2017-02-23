16b6191208765e5ab19c1e1013243f5093418393
estherleah <estherleahmorrison@gmail.com>
2017-02-23 16:02:19 +0000

    add frontend new user

--- wwwroot/signup.php
+++ wwwroot/signup.php
@@ -25,6 +25,8 @@ include_once 'database/database.php';
 <script src="js/jquery.min.js"></script>
 <!-- Include all compiled plugins (below), or include individual files as needed -->
 <script src="js/bootstrap.min.js"></script>
+<script src="js/bootstrap-formhelpers.min.js"></script>
+<script src="js/bootstrap-formhelpers-countries.js"></script>
 
 <!-- Content -->
 <div class="container">
@@ -35,23 +37,47 @@ include_once 'database/database.php';
                 <form method="post" action="includes/newuser.php" name="newUserForm">
                     <div class="form-group">
                         <label for="firstName">First Name:</label>
-                        <input class="form-control" type="text" name="firstName" id="firstName" placeholder="First name">
+                        <input class="form-control" type="text" name="firstName" id="firstName" placeholder="First name" required>
                     </div>
                     <div class="form-group">
                         <label for="lastName">Last Name:</label>
-                        <input class="form-control" type="text" name="lastName" id="lastName" placeholder="Last name">
+                        <input class="form-control" type="text" name="lastName" id="lastName" placeholder="Last name" required>
                     </div>
                     <div class="form-group">
                         <label for="email">Email:</label>
-                        <input class="form-control" type="email" name="email" id="email" placeholder="Email">
+                        <input class="form-control" type="email" name="email" id="email" placeholder="Email" required>
                     </div>
                     <div class="form-group">
                         <label for="password">Password:</label>
-                        <input class="form-control" type="password" name="password" id="password" placeholder="Password">
+                        <input class="form-control" type="password" name="password" id="password" placeholder="Password" required>
                     </div>
                     <div class="form-group">
                         <label for="confirmPassword">Confirm password:</label>
-                        <input class="form-control" type="password" name="confirmPassword" id="confirmPassword" placeholder="Confirm password">
+                        <input class="form-control" type="password" name="confirmPassword" id="confirmPassword" placeholder="Confirm password" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="location">Location:</label>
+                        <select class="form-control input-medium bfh-countries" name="location" id="location" data-country="GB"></select>
+                    </div>
+                    <div class="form-group">
+                        <label for="dob">Date of birth:</label>
+                        <div id="dob">
+                            <input class="form-control" type="date" id="dob" name="dob">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="gender">Gender:</label>
+                        <div id="gender">
+                            <label class="radio-inline">
+                                <input type="radio" name="gender" value="male">Male
+                            </label>
+                            <label class="radio-inline">
+                                <input type="radio" name="gender" value="female">Female
+                            </label>
+                            <label class="radio-inline">
+                                <input type="radio" name="gender" value="other">Other
+                            </label>
+                        </div>
                     </div>
                     <p><input class="btn btn-default" type="submit" value="Sign Up"></a></p>
                 </form>
