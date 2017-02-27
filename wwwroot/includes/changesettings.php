388a0a25ded4681c8a81bbb055dfcc8e232daf39
estherleah <estherleahmorrison@gmail.com>
2017-02-27 11:50:43 +0000

    frontend for settings page

---
+++ wwwroot/includes/changesettings.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Esther Leah
+ * Date: 27/02/2017
+ * Time: 11:44
+ */
--- wwwroot/settings.php
+++ wwwroot/settings.php
@@ -34,6 +34,63 @@ include 'header.php';
         <div class="text-center">
             <div class="col-sm-8 col-sm-offset-2">
                 <h2>Settings</h2>
+                <form method="post" action="includes/changesettings.php" name="settingsForm">
+                    <div class="form-group">
+                        <label for="firstName">First Name:</label>
+                        <input class="form-control" type="text" name="firstName" id="firstName" placeholder="First name" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="lastName">Last Name:</label>
+                        <input class="form-control" type="text" name="lastName" id="lastName" placeholder="Last name" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Email:</label>
+                        <input class="form-control" type="email" name="email" id="email" placeholder="Email" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password:</label>
+                        <input class="form-control" type="password" name="password" id="password" placeholder="Password" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="confirmPassword">Confirm password:</label>
+                        <input class="form-control" type="password" name="confirmPassword" id="confirmPassword" placeholder="Confirm password" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="privacy">Privacy:</label>
+                        <select class="form-control" name="privacy" id="privacy">
+                            <option value="public">Public</option>
+                            <option value="friends of friends">Friends of friends</option>
+                            <option value="circle">Circle</option>
+                            <option value="friends">Friends</option>
+                            <option value="private">Private</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="location">Location:</label>
+                        <select class="form-control input-medium bfh-countries" name="location" id="location" <!--data-country="GB"-->></select>
+                    </div>
+                    <div class="form-group">
+                        <label for="dob">Date of birth:</label>
+                        <div id="dob">
+                            <input class="form-control" type="date" id="dob" name="dob">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="gender">Gender:</label>
+                        <div id="gender">
+                            <label class="radio-inline">
+                                <input type="radio" name="gender" value="male">Male
+                            </label>
+                            <label class="radio-inline">
+                                <input type="radio" name="gender" value="female">Female
+                            </label>
+                            <label class="radio-inline">
+                                <input type="radio" name="gender" value="other">Other
+                            </label>
+                        </div>
+                    </div>
+                    <p><input class="btn btn-default" type="submit" value="Sign Up"></a></p>
+                </form>
             </div>
         </div>
     </div>
