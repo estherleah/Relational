6a5828aae74b0ce543d49cf7041573e2ddd9f4be
Esther Leah <estherleahmorrison@gmail.com>
2017-03-05 17:38:35 +0000

    Update chatProcess.php

--- wwwroot/includes/chatProcess.php
+++ wwwroot/includes/chatProcess.php
@@ -1,28 +1,24 @@
 <?php
-include_once '../database/database.php';
 session_start();
 $user = $_SESSION['user'];
 $name = $_SESSION['name'];
-//the problem isn't here either I think
-/*
-if (!isset($_POST['post']) or trim($_POST['post']) == '') {
-    echo "Please enter some text. (PHP)";
-    header("Location: ../chat.php");
+$circleID = $_SESSION['circleID'];
+
+$message = $_POST["message"];
+$date = date("Y-m-d H:i:s");
+
+$userIdEscaped = mysqli_real_escape_string($conn, $user);
+$messageEscaped = mysqli_real_escape_string($conn, $message);
+$dateEscaped = mysqli_real_escape_string($conn, $date);
+$circleIDEscaped = mysqli_real_escape_string($conn, $circleID);
+
+$messageInsertSql = "INSERT INTO message (userID, circleID, message, date)
+-                    VALUES ('$userIdEscaped', '$circleIDEscaped','$messageEscaped', '$dateEscaped')";
+
+if (mysqli_query($conn, $messageInsertSql)) {
+echo "Message sent";
+} else {
+echo "Error: " . $messageInsertSql . "<br>" . mysqli_error($conn);
 }
-else {
-    // temp until we maintain state
-    */
-    $message = $_POST["message"];
-    $date = date("Y-m-d H:i:s");
-    $userIdEscaped = mysqli_real_escape_string($conn, $user);
-    $messageEscaped = mysqli_real_escape_string($conn, $message);
-    $dateEscaped = mysqli_real_escape_string($conn, $date);
-    $messageInsertSql = "INSERT INTO message (userID, message, date)
-                    VALUES ('$userIdEscaped', '$messageEscaped', '$dateEscaped')";
-    //this isn't the problem
-    if (mysqli_query($conn, $messageInsertSql)) {
-        echo "Message sent";
-    } else {
-        echo "Error: " . $messageInsertSql . "<br>" . mysqli_error($conn);
-    }
+
 ?>
