822b2c3b03d7be0111df39dee284eddd7a2cd342
Sherri <leejiayusherri@gmail.com>
2017-03-07 13:37:46 +0000

    Fixed the circlechat bug - messages weren't displaying, should also work properly in master now

--- wwwroot/includes/initialiseCircleChat.php
+++ wwwroot/includes/initialiseCircleChat.php
@@ -1,17 +1,12 @@
 <?php
-
-
 //I don't think the problem is here either
 $userIDEscaped = mysqli_real_escape_string($conn, $user);
 $circleIDEscaped = mysqli_real_escape_string($conn, $circleID);
-
-
 $userSql = "SELECT firstName, lastName, profilephotoURL
               FROM user
               WHERE userID = '$userIDEscaped';
               ";
 $userResult = mysqli_query($conn, $userSql);
-
 if (mysqli_num_rows($userResult) === 1) {
     $row = mysqli_fetch_assoc($userResult);
     $fullName = $row["firstName"] . " " . $row["lastName"];
@@ -20,15 +15,12 @@ if (mysqli_num_rows($userResult) === 1) {
 //select the message database
 //attempt a different query - want to get everything ever posted in circle/chat
 //ON b.userID = '$userIDEscaped' AND b.userID = u.userID
-
 $messageSQL = "SELECT message, date, profilephotoURL, firstName, lastName, circleID
               FROM message
                JOIN user
               ON message.userID = user.userID
                WHERE circleID = '$circleIDEscaped'
-
               ORDER BY date ASC;
               ";
-
 $messageResult = mysqli_query($conn, $messageSQL);
 ?>
