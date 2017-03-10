0e150a56e08d2efb79e795d93617e2041c2773e4
Rob Farthing <farthir@outlook.com>
2017-03-10 21:56:41 +0000

    Restructure login

--- wwwroot/index.php
+++ wwwroot/index.php
@@ -1,54 +1,41 @@
 <?php
 include_once 'database/database.php';
-session_start();
-/**
- * Login page
- * User: Esther Leah
- * Date: 24/01/2017
- * Time: 22:11
- */
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Login</title>
-    <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+  <title>Login</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-<script src="js/jquery.min.js"></script>
-<!-- Include all compiled plugins (below), or include individual files as needed -->
-<script src="js/bootstrap.min.js"></script>
-
-<!-- Content -->
-<div class="container">
-    <div class="col-*-*">
-        <div class="text-center">
-            <div class="col-sm-4 col-sm-offset-4">
-                <h2>Login</h2>
-                <form method="post" action="includes/login.php" name="loginForm">
-                    <div class="form-group">
-                        <label for="email">Email:</label>
-                        <input class="form-control" type="email" name="email" id="email" placeholder="Email">
-                    </div>
-                    <div class="form-group">
-                        <label for="password">Password:</label>
-                        <input class="form-control" type="password" name="password" id="password" placeholder="Password">
-                    </div>
-                    <p><input class="btn btn-default" type="submit" value="Login"></p>
-                </form>
-                <p>Not yet a user? Sign up now.</p>
-                <a href="signup.php"><input class="btn btn-default" type="button" value="Sign up"></a>
-            </div>
-        </div>
+  <!-- Content -->
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-4 col-xs-offset-4 text-center">
+          <h2>Login</h2>
+          <form method="post" action="includes/login.php" name="loginForm">
+              <div class="form-group">
+                  <label for="email">Email:</label>
+                  <input class="form-control" type="email" name="email" id="email" placeholder="Email">
+              </div>
+              <div class="form-group">
+                  <label for="password">Password:</label>
+                  <input class="form-control" type="password" name="password" id="password" placeholder="Password">
+              </div>
+              <p><input class="btn btn-default" type="submit" value="Login"></p>
+          </form>
+          <p>Not yet a user? Sign up now.</p>
+          <a href="signup.php"><input class="btn btn-default" type="button" value="Sign up"></a>
+      </div>
     </div>
-</div>
+  </div>
+
+  <script src="js/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
 </body>
 </html>
