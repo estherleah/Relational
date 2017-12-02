6b10051a63980f5ed26f672a9a7cf68ed38aafd0
estherleah <estherleahmorrison@gmail.com>
2017-12-02 20:05:25 +0000

    Tidying up

--- 00_snippets/add_values.sql
+++
@@ -1,8 +0,0 @@
-USE social_network;
-
-INSERT INTO `privacy_settings` (`privacyID`, `option`) VALUES
-(1, 'public'),
-(2, 'friends of friends'),
-(3, 'circles'),
-(4, 'friends'),
-(5, 'private');
--- 00_snippets/updateOrigin.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `friendship` CHANGE `origin` `originUserID` INT(11) NOT NULL;
-ALTER TABLE `friendship` ADD CONSTRAINT `friendship_ibfk_3` FOREIGN KEY (`originUserID`) REFERENCES `user`(`userID`) ON DELETE RESTRICT ON UPDATE RESTRICT;
